58fda303be65eb321ebf6940f567733b4c3ef589
Sam Hunt <sam@poggle.org>
2024-10-05 19:40:56 +0100

    Finish making game
    - Asks for user input 5 times
    
    - Outputs scores and draw count.

--- javascript.js
+++ javascript.js
@@ -1,4 +1,9 @@
-console.log("Hello world");
+let humanScore = 0;
+let computerScore = 0;
+let drawCount = 0;
+
+let userChoice;
+let computerChoice;
 
 function getComputerChoice() {
     let choice = Math.random() * 3;
@@ -12,5 +17,65 @@ function getComputerChoice() {
 }
 
 function getHumanChoice() {
-    return prompt("Enter your choice: rock, paper, scissors.")
+    return prompt("Enter your choice: rock, paper, scissors.").toLowerCase();
 }
+
+function playRound() {
+    userChoice = getHumanChoice();
+    computerChoice = getComputerChoice();
+    console.log(`My choice: ${computerChoice}, your choice: ${userChoice}.`)
+    if (userChoice === computerChoice) {
+        console.log("Draw!");
+        drawCount++;
+    } else if (userChoice === "rock") {
+        processUserChoiceRock();
+    } else if (userChoice === "paper") {
+        processUserChoicePaper();
+    } else if (userChoice === "scissors") {
+        processUserChoiceScissors();
+    } else {
+        console.log("Invalid user input, processing as draw..");
+        drawCount++;
+    }
+}
+
+function processUserChoiceRock() {
+    if (computerChoice === "paper") {
+        console.log("Lost this round..");
+        computerScore++;
+    } else if (computerChoice === "scissors") {
+        console.log("Won this round!");
+        humanScore++;
+    }
+}
+
+function processUserChoicePaper() {
+    if (computerChoice === "rock") {
+        console.log("Won this round!");
+        humanScore++;
+    } else if (computerChoice === "scissors") {
+        console.log("Lost this round..");
+        computerScore++;
+    }
+}
+
+function processUserChoiceScissors() {
+    if (computerChoice === "rock") {
+        console.log("Lost this round..");
+        computerScore++;
+    } else if (computerChoice === "paper") {
+        console.log("Won this round!");
+        humanScore++;
+    }
+}
+
+function playGame() {
+    playRound();
+    playRound();
+    playRound();
+    playRound();
+    playRound();
+    console.log(`My score: ${computerScore}, your score: ${humanScore}. Draw count: ${drawCount}`)
+}
+
+playGame();
